b38d26677634c112a2623dcbe89a89e3a3908e49
kelle <kellerhydle@gmail.com>
2025-03-09 15:35:28 -0600

    App Updates
    -added app.js file

---
+++ src/app.js
@@ -0,0 +1,4 @@
+import * as THREE from 'three';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { TransformControls } from 'three/addons/controls/TransformControls.js';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
